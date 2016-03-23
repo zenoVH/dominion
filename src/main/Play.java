529f655ad4a1bf92da9e34b7f7013ba361dd8071
diddy696 <jaspy_007@hotmail.com>
2016-03-23 08:30:06 +0100

    webbased
    start

---
+++ src/main/Play.java
@@ -0,0 +1,56 @@
+
+package main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * Servlet implementation class DominionServlet
+ */
+public class Play extends HttpServlet {
+
+
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// Method to handle GET method request.
+	  public void doGet(HttpServletRequest request,
+	                    HttpServletResponse response)
+	            throws ServletException, IOException
+	  {
+	      // Set response content type
+	      response.setContentType("text/html");
+
+	      PrintWriter out = response.getWriter();
+		  String title = "Reading Checkbox Data";
+	      String docType =
+	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+	      "transitional//en\">\n";
+	      out.println(docType +
+	                "<html>\n" +
+	                "<head><title>" + title + "</title></head>\n" +
+	                "<body bgcolor=\"#f0f0f0\">\n" +
+	                "<h1 align=\"center\">" + title + "</h1>\n" +
+	                "<ul>\n" +
+	                "  <li><b>Maths Flag : </b>: "
+	                + request.getParameter("maths") + "\n" +
+	                "  <li><b>Physics Flag: </b>: "
+	                + request.getParameter("physics") + "\n" +
+	                "  <li><b>Chemistry Flag: </b>: "
+	                + request.getParameter("chemistry") + "\n" +
+	                "</ul>\n" +
+	                "</body></html>");
+	  }
+	  // Method to handle POST method request.
+	  public void doPost(HttpServletRequest request,
+	                     HttpServletResponse response)
+	      throws ServletException, IOException {
+	     doGet(request, response);
+	  }
+
+}
